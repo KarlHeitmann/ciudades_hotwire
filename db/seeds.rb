80b0d1fd25065ec5ea873c539cd4be4923834d81
Karl Heitmann <khf1988@gmail.com>
2021-05-08 11:51:28 -0400

    colocadas unas semillas, creado parcial _country.html.erb para renderizarse en index.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+chile = Country.create!(name: "Chile", population: 17574003)
+chile = Country.create!(name: "Costa Rica", population: 5094118)
